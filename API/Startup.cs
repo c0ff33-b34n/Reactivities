17a48deadf42f5e219e4efb0c46b8fcd2bd26fd8
Stuart Bowes <stuartbowes2000@gmail.com>
2020-02-10 13:56:35 +0000

    Add CORS to API for browser to allow client(app) access to data from another domain.

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,14 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -48,6 +56,8 @@ namespace API
             app.UseRouting(); // This middleware enables that functionality.
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
