d5e5d313bab6b9f3a7f2406977fdba2a4edb7afb
Stuart Bowes <stuartbowes2000@gmail.com>
2020-02-11 19:41:11 +0000

    Handling form submission - ActivityForm

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import { Header, Icon, List, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import axios from 'axios';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
@@ -19,6 +19,18 @@ const App = () => {
     setEditMode(true);
   }
 
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
   useEffect(() => {
     axios
       .get<IActivity[]>('http://localhost:5000/api/activities')
@@ -33,7 +45,10 @@ const App = () => {
       <Container style={{marginTop: '7em'}}>
         <ActivityDashboard activities={activities} selectActivity={handleSelectActivity}
           selectedActivity={selectedActivity} editMode={editMode} setEditMode={setEditMode}
-          setSelectedActivity={setSelectedActivity}/>
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          />
       </Container>
     </Fragment>
   );
