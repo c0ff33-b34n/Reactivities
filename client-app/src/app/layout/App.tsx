987c3353c9f4bee3fd5080178d04449de6c72116
Stuart Bowes <stuartbowes2000@gmail.com>
2020-02-12 10:40:38 +0000

    Add delete functionality to activities - ActivityDashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -32,6 +32,10 @@ const App = () => {
     setEditMode(false);
   }
 
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !== id)])
+  }
+
   useEffect(() => {
     axios
       .get<IActivity[]>('http://localhost:5000/api/activities')
@@ -54,6 +58,7 @@ const App = () => {
           setSelectedActivity={setSelectedActivity}
           createActivity={handleCreateActivity}
           editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
           />
       </Container>
     </Fragment>
