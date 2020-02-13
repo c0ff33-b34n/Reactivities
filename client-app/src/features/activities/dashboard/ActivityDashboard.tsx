939cb6e0a1c2082794f000506d413e44d0ac8dd7
Stuart Bowes <stuartbowes2000@gmail.com>
2020-02-13 13:57:01 +0000

    Add routing to client-app

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,29 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
-import ActivityStore from '../../../app/stores/activityStore';
-import ActivityDetails from '../details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
+import ActivityStore from '../../../app/stores/activityStore';
+import { LoadingComponent } from '../../../app/layout/LoadingComponent';
 
 const ActivityDashboard: React.FC = () => {
   const activityStore = useContext(ActivityStore);
-  const {editMode, selectedActivity} = activityStore;
+
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]); // empty array [] as second parameter ensures useEffect only runs once and not continuously.
+  // [activityStore] in the array is required to notify useEffect of it's dependency (for functions)
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading Activities" />;
   return (
     <Grid>
       <Grid.Column width={10}>
         <ActivityList />
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && !editMode && (
-          <ActivityDetails />
-        )}
-        {editMode && (
-          <ActivityForm
-            key={(selectedActivity && selectedActivity.id) || 0}
-            activity={selectedActivity!}
-          />
-        )}
+        <h2>Activity filters</h2>
       </Grid.Column>
     </Grid>
   );
