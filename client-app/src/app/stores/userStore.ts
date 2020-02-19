e09a2a2a94d15b763a9e99f3da3b51994c7736be
Stuart Bowes <stuartbowes2000@gmail.com>
2020-02-19 14:25:02 +0000

    Add modal register form and display validation errors - client-app

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -20,8 +20,19 @@ export default class UserStore {
             runInAction(() => {
                 this.user = user;
             })
-            this.rootStore.modalStore.closeModal();
             this.rootStore.commonStore.setToken(user.token);
+            this.rootStore.modalStore.closeModal();
+            history.push('/activities');
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    @action register = async (values: IUserFormValues) => {
+        try {
+            const user = await agent.User.register(values);
+            this.rootStore.commonStore.setToken(user.token);
+            this.rootStore.modalStore.closeModal();
             history.push('/activities');
         } catch (error) {
             throw error;
