02762807ab582e94626f6cba7e0069105324028a
Stuart Bowes <stuartbowes2000@gmail.com>
2020-02-19 13:42:30 +0000

    Create ErrorMessage component and add to login form - client-app

--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import { Form as FinalForm, Field } from 'react-final-form';
-import { Form, Button, Label, Header } from 'semantic-ui-react';
+import { Form, Button, Header } from 'semantic-ui-react';
 import TextInput from '../../app/common/form/TextInput';
 import { RootStoreContext } from '../../app/stores/rootStore';
 import { IUserFormValues } from '../../app/models/user';
 import { FORM_ERROR } from 'final-form';
 import { combineValidators, isRequired } from 'revalidate';
+import ErrorMessage from '../../app/common/form/ErrorMessage';
 
 const validate = combineValidators({
     email: isRequired('email'),
@@ -22,11 +23,11 @@ const LoginForm = () => {
       }))}
       validate={validate}
       render={({ handleSubmit, submitting, submitError, invalid, pristine, dirtySinceLastSubmit }) => (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} error>
           <Header as='h2' content='Login to Reactivities' color='teal' textAlign='center' />
           <Field name='email' component={TextInput} placeholder='Email' />
           <Field name='password' component={TextInput} placeholder='Password' type='password' />
-          {submitError && !dirtySinceLastSubmit && (<Label color='red' basic content={submitError.statusText}/>)}
+          {submitError && !dirtySinceLastSubmit && (<ErrorMessage error={submitError} text='Invalid email or password' />)}
           <Button disabled={(invalid && !dirtySinceLastSubmit) || pristine} loading={submitting} color='teal' content='Login' fluid/>
         </Form>
       )}
